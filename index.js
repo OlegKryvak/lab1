500194110e679034ce2eeae292101c3605c23a29
OlehKryvak <oleh.kryvak@flysoft-tech.com>
2022-10-31 22:24:14 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var obj = {
+    name: 'Anton',
+    age: 18
+};
+console.log('kw');
---
+++ index.ts
@@ -0,0 +1,11 @@
+type User = {
+    name: string,
+    age: number
+}
+
+const obj = {
+    name: 'Oleh',
+    age: 18,
+}
+
+console.log('kw');
